58f05c15566aaedbc752cffa5df255932cbced28
SiddhiRastogi <68500051+SiddhiRastogi@users.noreply.github.com>
2020-08-22 12:21:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var car,wall
+var speed,weight
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
